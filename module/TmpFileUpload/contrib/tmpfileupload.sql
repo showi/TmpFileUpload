1182f0988d5c7e8c4b259d3f907943a9b00b4481
showi <joachim.basmaison@gmail.com>
2014-08-21 11:09:00 +0200

    Adding GPL2 License

--- module/TmpFileUpload/contrib/tmpfileupload.sql
+++ module/TmpFileUpload/contrib/tmpfileupload.sql
@@ -1,11 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 4.2.6
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Lun 28 Juillet 2014 à 18:07
--- Version du serveur :  10.0.12-MariaDB-log
--- Version de PHP :  5.5.15
+--/*
+--* Copyright (c) 2014 Joachim Basmaison
+--*
+--* This program is free software; you can redistribute it and/or modify it
+--* under the terms of the GNU General Public License as published by
+--* the Free Software Foundation; either version 2 of the License,
+--* or (at your option) any later version. This program is distributed in the
+--* hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+--* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+--*
+--* See the GNU General Public License for more details.
+--*/
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
